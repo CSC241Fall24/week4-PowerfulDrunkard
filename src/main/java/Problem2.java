f292e50985b341e442ffd1c9c150752218f2a41b
PowerfulDrunkard <MS1031902@wcupa.edu>
2024-09-26 15:24:49 -0400

    Testing Week 4 Assignment

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,9 +1,14 @@
 
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
-        // TODO: Implement the insert method
-        // This method should insert a new node with the given value at the specified position
-        // If the position is out of bounds, insert the node at the end of the list
-        return null;
+        ListNode current = head;
+        for(int i = 0; i < position; i++){
+        current=current.next;
+        }
+        ListNode insert = new ListNode(val);
+        insert.next = current.next;
+        current.next = insert;
+
+        return head;
     }
 }
