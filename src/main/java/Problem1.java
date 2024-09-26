48069120d73e2cfeea5e70659e9210eed4e5eb27
PowerfulDrunkard <MS1031902@wcupa.edu>
2024-09-26 16:17:33 -0400

    added comments

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,16 +1,19 @@
 
 public class Problem1 {
     public static ListNode concatenate(ListNode l1, ListNode l2) {
+        //basecase for if l1 is empty
         if (l1 == null) {
             return l2;}
+        //basecase for if l2 is empty
         if (l2 == null) {
             return l1;}
-        ListNode currentl1 = l1;
+        ListNode currentl1=l1;
         ListNode currentl2=l2;
+        //finds the tail node of l1 and sets currentl1 to it
         while (currentl1.next != null) {
             currentl1 = currentl1.next;
         }
-        
+        //Cycles through the l2 clone current l2 and makes a copy of each node to attach to currentl1, the tail of l1
         while(currentl2 != null){
             currentl1.next=new ListNode(currentl2.val);
             currentl2=currentl2.next;
--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -1,21 +1,27 @@
 
 public class Problem2 {
     public static ListNode insert(ListNode head, int val, int position) {
+        //create a new node with the given value
         ListNode insert = new ListNode(val);
+        //check to make sure the NodeList isn't empty
         if(head==null){
             return new ListNode(val);}
+        //basecase if it is put first
         if(position == 1){
             insert.next=head;
             return insert;
         }
+        //cycles through and finds specified position
         ListNode current = head;
-        for(int i = 2; i < position; i++){
+        for(int i = 1; i < position-1; i++){
         if(current.next != null){
         current=current.next;
         }
         }
+        //links the previous positions next to insert, and links insert to the previous position 
         insert.next = current.next;
         current.next = insert;
+        //returns modified linked list
         return head;
     }
 }
